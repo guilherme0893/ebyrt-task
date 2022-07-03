3547ec6565fb69183e21c1011bdd785d7d9103a5
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-03 01:16:09 -0300

    refact: refactors App to Class and adds cors, and taskRouter

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,10 +1,42 @@
 import express, { Request, Response } from 'express';
+import TaskRouter from './routes/tasks';
+import cors from 'cors';
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get('/', (_request: Request, response: Response) =>
-  response.send({ status: 'I am alive!' }));
+// app.get('/', (_request: Request, response: Response) =>
+//   response.send({ status: 'I am alive!' }));
 
-export default app;
+// export default app;
+
+class App {
+  public app: express.Express;
+  public task = new TaskRouter();
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(accessControl);
+    this.task.task(this.app);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT);
+    console.log(PORT);
+  }
+}
+
+export { App };
