04c524608e5be38a23b56e51eb4db5cf53a592ef
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-03 01:16:53 -0300

    refact: refactors index following App refactoring to class

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,8 +1,10 @@
-import app from './app';
+import { App } from './app';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+new App().start(PORT);
+
+// App.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
