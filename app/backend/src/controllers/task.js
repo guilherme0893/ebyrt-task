d89a58dd9f08b4bd4df08dc24ffb5196f0f09dc8
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-04 17:31:48 -0300

    chore: creates createTask in controller

--- app/backend/src/controllers/task.js
+++ app/backend/src/controllers/task.js
@@ -1,14 +1,14 @@
 const taskService = require('../services/task');
 
-const getAll = async(_req, res) => {
+const getAll = async (_req, res) => {
   const tasks = await taskService.getAll();
   return res.status(200).json(tasks);
 };
 
 const createTask = async (req, res) => {
   const { task, taskStatus } = req.body;
-  const newTask = await taskService.createTask({ task, taskStatus });
-  return res.status(201).json({newTask});
+  const newTask = await taskService.createTask(task, taskStatus);
+  return res.status(201).json(newTask);
 };
 
 module.exports = {
