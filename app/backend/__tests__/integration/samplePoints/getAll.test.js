9fe326a13b649b8f4c80079abef20d41a40e481f
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-04 15:32:59 -0300

    tests: starts refact back to JS, updates tasks tests

---
+++ app/backend/__tests__/integration/samplePoints/getAll.test.js
@@ -0,0 +1,31 @@
+const  sinon = require('sinon');
+const  chai = require('chai');
+const { expect } = chai;
+const chaiHttp = require('chai-http');
+const taskModel = require('../../../src/models/task');
+const app = require('../../../src/app')
+const { taskMock } = require('../../mocks/tasksMocks/taskMock');
+
+chai.use(chaiHttp)
+
+describe('Tests the GET "/" endpoint', () => {
+  let chaiHttpResponse;
+  
+  beforeEach(async () => {
+    sinon.stub(taskModel, 'getAll').resolves(taskMock);
+  });
+
+  afterEach(() => {
+    taskModel.getAll.restore();
+  });
+
+  it('if successful, returns all tasks and a status 200', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/')
+    console.log(chaiHttpResponse.body);
+    console.log(taskMock);
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(taskMock)
+  });
+});
