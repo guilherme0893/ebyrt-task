4142c81b781e5cb906659bf7344f58b2e88712b8
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-04 17:43:09 -0300

    chore: adds cors

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const user = require('./routes/user');
+const cors = require('cors');
 const taskRouter = require('./routes/tasks');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/', taskRouter);
 
