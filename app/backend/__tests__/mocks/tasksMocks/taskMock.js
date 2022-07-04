065c42d4edb6ae9dfa1202948cc831c14b055410
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-04 15:37:08 -0300

    chore: creates taskMock files

---
+++ app/backend/__tests__/mocks/tasksMocks/taskMock.js
@@ -0,0 +1,23 @@
+const taskMock = [
+  {
+    id: 1,
+    task: 'Wake up',
+    taskStatus: 'finished',
+  },
+  {
+    id: 2,
+    task: 'Brush teeth',
+    taskStatus: 'ongoing',
+  }
+];
+
+const singleTaskMock = {
+  id: 1,
+  task: 'Sleep',
+  taskStatus: 'ongoing',
+};
+
+module.exports = {
+  taskMock,
+  singleTaskMock,
+};
