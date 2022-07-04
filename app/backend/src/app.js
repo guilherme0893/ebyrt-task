2f299664ab44bd25bdc4ee4038382b93261a1d07
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-04 17:32:55 -0300

    chore: adds bodyParser instead of express.json

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 // const cors = require('cors');
 // const user = require('./routes/user');
 const taskRouter = require('./routes/tasks');
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.use('/', taskRouter);
 
